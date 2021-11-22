6ac218d832d3dc0bd5aec5a309d0eacc9ede64f2
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-11-22 17:17:44 +0100

    added some validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
-has_many :preparation_steps
-has_many :recipes_lists
+  has_many :preparation_steps
+  has_many :recipes_lists
+  validates :name, length: { maximum: 100,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :description, length: { maximum: 500,
+    too_long: "%{count} characters is the maximum allowed" }
 end
