56abffec5578fd596ec76ba3a3b4b0253b578800
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-11-22 15:51:29 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :recipes, only: [:index, :show]
+
+  resources :batch_menus, only: [:new, :create, :show] do
+    member do
+      patch :associate_recipes # ici on affichera l'index des recettes pour les associer
+    end
+    # patch :associate_recipes, on: :member # ici on affichera l'index des recettes pour les associer
+  end
 end
