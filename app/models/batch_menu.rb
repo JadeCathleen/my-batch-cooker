8cab6cd0cf347fd9d96d2d5e5d6f9dad7166bb82
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-11-22 17:54:53 +0100

    added valids +  enum

--- app/models/batch_menu.rb
+++ app/models/batch_menu.rb
@@ -3,5 +3,6 @@ class BatchMenu < ApplicationRecord
   validates :name, length: { maximum: 60,
     too_long: "%{count} characters is the maximum allowed" }
   validates :nb_of_people, numericality: { less_than: 5 }
+  validates :nb_of_meals, numericality: { less_than: 15 }
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,20 @@
 class Ingredient < ApplicationRecord
   has_many :ingredient_qtys
+    enum category: {
+    meat: 0,
+    poultry: 1,
+    fish: 2,
+    cheese: 3,
+    fruit: 4,
+    condiment: 5,
+    vegetable: 6,
+    liquid: 7
+  }
+
+  enum category: {
+    g: 0,
+    cl: 1,
+    teaspoon: 2,
+    tablespoon: 3
+  }
 end
