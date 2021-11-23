31359940be51753ae4340b66f0a12d9db748e801
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-11-23 12:00:23 +0100

    redid valids and enums

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,23 @@
 class Ingredient < ApplicationRecord
   has_many :ingredient_quantities
+  enum category: {
+    "boucherie" => 1,
+    "volaille" => 2,
+    "poissonnerie" => 3,
+    "crèmerie" => 4,
+    "fruit" => 5,
+    "épicerie" => 6,
+    "légumes" => 7,
+    "liquides" => 8,
+    "rayon_frais" => 9
+  }
+
+  enum unit_category: {
+    "pièce" => 1,
+    'g' => 2,
+    "cl" => 3,
+    "c.à café" => 4,
+    "c. à soupe" => 5,
+    "pincée" => 6
+  }
 end
