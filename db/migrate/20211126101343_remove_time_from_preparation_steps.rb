b1e7a54317483e9c37bce1ab549ec6b3531c4297
Jade Cormier <cormier.jade@gmail.com>
2021-11-26 13:08:42 +0100

    Front show ok

--- db/migrate/20211126101343_remove_time_from_preparation_steps.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTimeFromPreparationSteps < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :preparation_steps, :time, :time
-  end
-end
--- db/migrate/20211126101634_add_time_to_preparation_steps.rb
+++
@@ -1,5 +0,0 @@
-class AddTimeToPreparationSteps < ActiveRecord::Migration[6.0]
-  def change
-    add_column :preparation_steps, :time, :integer
-  end
-end
