481a7230a38e118959cddcce819ab1249dd6bf9e
Jade Cormier <cormier.jade@gmail.com>
2021-11-30 13:03:33 +0100

    front panier recettes + bouton valider disabled ok

--- app/javascript/controllers/recipe_selection_controller.js
+++ app/javascript/controllers/recipe_selection_controller.js
@@ -10,7 +10,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["content", "counter"]
+  static targets = ["content", "counter", "recipeNumber"]
 
   selectRecipe(e) {
     const recipeId = e.currentTarget.dataset.id;
@@ -24,13 +24,23 @@ export default class extends Controller {
       body: formData
     }).then(response => response.json())
     .then((data) => {
-      this.contentTarget.insertAdjacentHTML('beforeend', `<p>${data.recipe_name} <a rel="nofollow" data-method="delete" href="/recipes_lists/${data.recipes_list_id}"><i class="fas fa-times"></i></a></p>`)
+
+      this.contentTarget.insertAdjacentHTML('beforeend', `<div class="chosen-recipe"><p>${data.recipe_name}</p> <a rel="nofollow" data-method="delete" href="/recipes_lists/${data.recipes_list_id}"><i class="fas fa-times"></i></a></div>`)
       let meals_left = meal - this.contentTarget.childElementCount;
-      if (meals_left > 1) {
-        this.counterTarget.innerText = ` ${meals_left} recettes`
-      } else {
-        this.counterTarget.innerText = ` ${meals_left} recette`
-      }
+      // do {
+        //   this.contentTarget.nextSibling.nextSibling.firstChild.classList.add("disabled")
+        // } while (meals_left > 0);
+        if (meals_left > 1) {
+          this.counterTarget.innerText = ` ${meals_left} recettes`
+        } else if (meals_left === 1) {
+          this.counterTarget.innerText = ` ${meals_left} recette`
+        } else {
+          this.recipeNumberTarget.innerHTML = '<em>Vous avez choisi toutes vos recettes ! 🎉</em>'
+        }
+      console.log(this.contentTarget.nextSibling.nextSibling)
+        if (meals_left === 0) {
+          this.contentTarget.nextSibling.nextSibling.classList.remove("disabled")
+        }
     })
   }
 }
